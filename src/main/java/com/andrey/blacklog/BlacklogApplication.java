abf0598cee94fa3a9bc00cfb1fe6af9aebbd14d4
andreyDelay <frowzygleb@gmail.com>
2022-07-06 10:52:51 +0300

    init commit

---
+++ src/main/java/com/andrey/blacklog/BlacklogApplication.java
@@ -0,0 +1,13 @@
+package com.andrey.blacklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlacklogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlacklogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrey/blacklog/BlacklogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrey.blacklog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlacklogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
