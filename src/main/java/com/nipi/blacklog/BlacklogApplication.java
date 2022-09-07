5345fb8c3ab09d98663621a2d08d6cc03e0a129e
andrey <frowzygleb@gmail.com>
2022-09-07 20:57:01 +0900

    init commit

---
+++ src/main/java/com/nipi/blacklog/BlacklogApplication.java
@@ -0,0 +1,13 @@
+package com.nipi.blacklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlacklogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlacklogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nipi/blacklog/BlacklogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nipi.blacklog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlacklogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
